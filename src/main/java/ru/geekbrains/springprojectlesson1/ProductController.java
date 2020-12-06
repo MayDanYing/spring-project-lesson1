25401ff9bc43d96a60118b89694e5a0ba6b557cc
MayDanYing <tasya757@gmail.com>
2020-12-06 18:43:31 +0300

    hm1

--- src/main/java/ru/geekbrains/springprojectlesson1/ProductController.java
+++ src/main/java/ru/geekbrains/springprojectlesson1/ProductController.java
@@ -3,6 +3,7 @@ package ru.geekbrains.springprojectlesson1;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @RestController
@@ -14,7 +15,6 @@ public class ProductController {
     public void setProductRepository(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
-
     @GetMapping
     public List<Product> getAllProducts() {
         return productRepository.findAll();
@@ -25,39 +25,30 @@ public class ProductController {
 
         return productRepository.findById(id).get();
     }
-
-    @PostMapping
-    public Product saveProduct(@RequestBody Product product) {
-        product.setId(null);
-        return productRepository.save(product);
-    }
-
-    @GetMapping("/increase_cost_by_10/{id}")
-    public void increaseCost(@PathVariable Long id) {
-        Product p = productRepository.findById(id).get();
-        p.setPrice(p.getPrice() + 10);
-        productRepository.save(p);
-    }
-
-    @GetMapping("/count")
-    public Long getProductsCount() {
-
-        return productRepository.count();
-    }
-
-    @GetMapping("/cheap")
-    public List<Product> getAllCheapProducts() {
-
-        return productRepository.requestAllCheapProducts();
-    }
-
     // 1. Обработать запрос вида: GET /products/filtered?min_price=100
     // В результате должен вернуться список товаров, цена которых >= 100
+    @GetMapping("/filtered")
+    public List<Product> getProductsWithPriceGreaterThan100(@RequestParam Integer min_price) {
+
+        return productRepository.requestProductsPriceGreaterThan(min_price);
+    }
+
     // 2. Обработать запрос вида: GET /products/delete/1
     // В результате должен удалиться товар с соответствющим id
+    @GetMapping("/delete/{id}")
+    public void deleteById(@PathVariable Long id) {
+
+       productRepository.deleteById(id);
+    }
+
     // 3. * Попробуйте реализовать возможность изменения названия товара по id
     // Что-то вроде: /products/{id}/change_title...
-
-
+    ///products/{id}/change_title?title=mars
+    @PutMapping("/{id}/change_title")
+    public Product changeTitle(@PathVariable Long id, @RequestParam String title) {
+        Product product = productRepository.findById(id).get();
+        product.setTitle(title);
+        return productRepository.save(product);
+    }
 
 }
